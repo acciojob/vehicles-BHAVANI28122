3bd3319d455cebe925819bbe195bf8f6fb957d08
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-17 14:48:14 +0530

    boat

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,19 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    String a ;
+    int b;
+    @Override
+    public String getVehicleName() {
+        this.a = a;
+        return a;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        this.b = b;
+        return b;
+    }
+
+}
