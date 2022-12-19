ed590eab4a6fabc28a33b2a2eae519b1198b6191
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-19 13:57:33 +0530

    do

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -21,29 +21,28 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        if(newSpeed <= 0 ) {
-
-            System.out.println("Stop the Car");
+        if(newSpeed == 0) {
+            stop();
             changeGear(1);
             //Stop the car, set gear as 1
         }
-        else if(newSpeed >= 1  && newSpeed <=50){
-            changeGear(1);
-        }
-        else if(newSpeed >=51 && newSpeed <=100){
+        else if(newSpeed > 50){
             changeGear(2);
         }
-        else if(newSpeed >= 101 && newSpeed <= 150){
+        else if(newSpeed > 100){
             changeGear(3);
         }
-        else if(newSpeed >= 151 && newSpeed <= 200){
+        else if(newSpeed > 150){
             changeGear(4);
         }
-        else if(newSpeed >= 201 && newSpeed <= 250){
+        else if(newSpeed > 200){
             changeGear(5);
         }
         else if(newSpeed > 250){
             changeGear(6);
+        }
+        else if(newSpeed > 0){
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -12,6 +12,17 @@ public class Vehicle {
         this.currentSpeed =  0;
         this.currentDirection = 0;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
     public String getName() {
         return name;
     }
@@ -28,6 +39,7 @@ public class Vehicle {
         this.currentDirection = direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
+
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
         this.currentSpeed = speed;
